0952fa8d7161fd45ffd464d9641aeca80cf08664
Ruslan Kotliarenko <ruslankot101@gmail.com>
2024-12-28 12:31:17 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// get env vars from vite app
+const API_URL = import.meta.env.VITE_API_URL + '/todos';
+
+const readTodos = async () => {
+  let res = await axios.get(API_URL);
+  return res.data;
+}
+
+const createTodo = async (name) => {
+  let res = await axios.post(API_URL, { 'name': name });
+  return res.data;
+}
+
+const doneTodo = async (id) => {
+  let res = await axios.put(`${API_URL}/${id}/done`);
+  return res.data;
+}
+
+const undoneTodo = async (id) => {
+  let res = await axios.delete(`${API_URL}/${id}/done`);
+  return res.data;
+}
+
+export {
+  readTodos,
+  createTodo,
+  doneTodo,
+  undoneTodo
+}
